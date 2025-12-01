a689b60ecd3ebab19819a597d59772a2ece5b69b
flaberpengu <53153483+flaberpengu@users.noreply.github.com>
2025-12-01 21:31:57 +0000

    Update Day 1 Challenge 1

--- day1/chall1/src/main.rs
+++ day1/chall1/src/main.rs
@@ -10,27 +10,41 @@ fn main() {
     let mut direction: Option<char>;
     let mut digit_str: String;
     let mut turn_amount: i32;
+    let mut increase_amount: u32;
     let mut num_times_zero: u32 = 0;
     for command in commands{
         chars = command.chars();
         direction = chars.next();
-        digit_str = chars.collect();
-        turn_amount = digit_str.parse::<i32>().unwrap();
-        if direction == Some('L'){
-            pos -= turn_amount;
-            while pos < 0{
-                pos += 100;
+        match direction{
+            None => {},
+            Some(x) => {
+                digit_str = chars.collect();
+                turn_amount = digit_str.parse::<i32>().unwrap();
+                
+                (pos, increase_amount) = turn(x, turn_amount, pos);
+                num_times_zero += increase_amount;
             }
-        }
-        else{
-            pos += turn_amount;
-            while pos >= 100{
-                pos -= 100;
-            }
-        }
-        if pos == 0{
-            num_times_zero += 1;
         }
     }
     println!("Password: {}", num_times_zero);
+}
+
+//Take in number of spaces to turn and a direction, return the updated position and the number of times it passed 0
+fn turn(direction: char, turn_amount: i32, mut pos: i32) -> (i32, u32){
+    if direction == 'L'{
+        pos -= turn_amount;
+        while pos < 0{
+            pos += 100;
+        }
+    }
+    else if direction == 'R'{
+        pos += turn_amount;
+        while pos >= 100{
+            pos -= 100;
+        }
+    }
+    if pos == 0 {
+        return (pos, 1);
+    }
+    return (pos, 0);
 }
